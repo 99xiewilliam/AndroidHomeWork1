f3aa52a43530f0048e2906a734ab74f2722510f9
99xiewilliam <35141949+99xiewilliam@users.noreply.github.com>
2021-09-28 10:40:55 +0800

    update code

--- app/src/main/java/hk/edu/cuhk/ie/iems5722/helloworld/MainActivity.java
+++ app/src/main/java/hk/edu/cuhk/ie/iems5722/helloworld/MainActivity.java
@@ -2,7 +2,10 @@ package hk.edu.cuhk.ie.iems5722.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
